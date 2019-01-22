05ede9dab3f435bd1d5593671c226fc912748be1
fanjiantao <1418154909@qq.com>
2019-01-22 15:53:37 +0800

    提交一次更新

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import Taro, { Component } from '@tarojs/taro'
 import Index from './pages/index'
 
 import './app.less'
-
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
 // if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
